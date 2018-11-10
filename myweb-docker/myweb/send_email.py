72cf0ec069d01fe8538bd7491dd9da1a3bfd92e9
[leemysw] <[leemysw@163.com]>
2018-11-10 18:49:22 +0800

    updata

--- myweb-docker/myweb/send_email.py
+++ myweb-docker/myweb/send_email.py
@@ -10,7 +10,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 def send_email(send_to, code):
     subject, from_email, to = '来自www.leemysw.top的验证邮件', 'leemysw@sina.com', send_to
     text_content = '欢迎访问www.leemysw.top'
-    html_content = '<p>感谢注册<a href="https://www.leemysw.top/confirm/?code={}" target=blank>www.leemysw.top</a>,</p>' \
+    html_content = '<p>感谢注册<a href="https://www.leemysw.top/user/?code={}" target=blank>www.leemysw.top</a>,</p>' \
                    '<p>此链接有效期为7天！</p>'.format(code)
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
